be1181277d26041828373eb6e07ac4ad9f32d2bf
KaliszS <skaliszoo@gmail.com>
2019-10-29 13:44:59 +0100

    Strona glowna oraz rejestracja z walidacja

---
+++ scripts/register.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    const GET = "GET";
+    const POST = "POST";
+
+    let registrationForm = document.getElementById("registration-form");
+    let loginInput = document.getElementById("login");
+
+    loginInput.addEventListener("change", checkLoginAvailability);
+
+    registrationForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log("zatrzymałem");
+
+        let loginInput = document.getElementById("login");
+        let baseUrl = "https://pi.iem.pw.edu.pl/user/";
+        let userUrl = baseUrl + loginInput.value;
+
+        fetch(userUrl, {method: GET}).then(function (resp) {
+            console.log(resp.status);
+            if( resp.status == 404) {
+                registrationForm.submit();
+            }
+            else {
+                alert('Login jest już zajęty');
+            }
+        }).catch(function (err) {
+            console.log(err);
+            return err.status;
+        });
+
+        var n = event.srcElement.length;
+        for (var i = 0; i < n; i++) {
+            console.log(event.srcElement[i].value);
+        }
+    });
+
+    function checkLoginAvailability() {
+        let loginInput = document.getElementById("login");
+        let baseUrl = "https://pi.iem.pw.edu.pl/user/";
+        let userUrl = baseUrl + loginInput.value;
+
+        fetch(userUrl, {method: GET}).then(function (resp) {
+            console.log(resp.status);
+            return resp.status;
+        }).catch(function (err) {
+            console.log(err);
+            return err.status;
+        });
+        console.log('string KRzyska')
+    }
+});
+
